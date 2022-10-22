93e8905f09b8ba7e91abf343dc17a8f9c89de77e
Gopikrishna Reddy <gopikrishnareddy198@gmail.com>
2022-10-21 21:51:00 -0500

    Added Role & UserRole class

---
+++ src/main/java/com/taskmanagementsytem/model/UserRole.java
@@ -0,0 +1,30 @@
+package com.taskmanagementsytem.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserRole implements GrantedAuthority {
+
+   private Role role;
+
+
+   public UserRole() {
+
+   }
+
+   public UserRole(Role role) {
+      this.role = role;
+   }
+
+   public Role getRole() {
+      return role;
+   }
+
+   public void setRole(Role role) {
+      this.role = role;
+   }
+
+   @Override
+   public String getAuthority() {
+      return role.getName();
+   }
+}
